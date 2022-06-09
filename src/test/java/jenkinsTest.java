929953db21d89b5f0a1456163b2e255642015810
listeh-lucena <liseth.lucena@globant.com>
2022-06-09 14:38:20 -0500

    Initial commit

---
+++ src/test/java/jenkinsTest.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class jenkinsTest {
+
+    @Test
+    public void primerTest(){
+        Assert.assertEquals(2,2);
+    }
+}
